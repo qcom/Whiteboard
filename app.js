6708923cd0b5dfba07ea0e592c146a12901fefcf
Zach <qcom100@gmail.com>
2011-08-28 21:40:50 -0700

    lol

--- app.js
+++ app.js
@@ -15,10 +15,14 @@ function compile (str, path) {
     .use(nib());
 };
 
-app.configure(function () {
-	app.set('views', __dirname + '/views');
-	app.set('view engine', 'jade');
-	app.use(stylus.middleware({ src: __dirname + '/public', compile: compile }))
+app.configure(function(){
+  app.set('views', __dirname + '/views');
+  app.set('view engine', 'jade');
+  app.use(express.bodyParser());
+  app.use(express.methodOverride());
+  app.use(express.cookieParser());
+  app.use(express.session({ secret: 'lulIpwnn00bsalldayerrydayfalife$', store: new RedisStore }));
+  app.use(require('stylus').middleware({ src: __dirname + '/public', compile: compile }));
   app.use(app.router);
   app.use(express.static(__dirname + '/public'));
 });
