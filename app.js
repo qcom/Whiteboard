346a715ebc1f874a011b383566401c1e03220e56
Zach <qcom100@gmail.com>
2011-08-28 21:33:40 -0700

    changed .listen()

--- app.js
+++ app.js
@@ -33,9 +33,8 @@ app.get('/', function (req, res) {
 
 // Listen
 
-app.listen(80, function () {
-  console.log('app listening on http://127.0.0.1:' + app.address.port);
-});
+app.listen(80);
+console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
 
 // Socket.IO
 
